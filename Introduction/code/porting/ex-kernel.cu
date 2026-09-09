98e14d4e3071cebec7c72b2bb219e335b6149bc6
Gokul Edakke Puram <ge-235725@hs-weingarten.de>
2026-09-09 23:49:03 +0200

    Port the work loop to a kernel
    Grid stride increase kernel plus its nsys profile.

---
+++ Introduction/code/porting/ex-kernel.cu
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <iomanip>
+#include <chrono>
+#include <thread>
+#include <cuda/cmath>
+#include <util.h>
+
+void increase(size_t* data, size_t numElements) {
+    for (int i = 0; i < numElements; ++i) {
+        data[i] += 1;
+    }
+}
+
+int main(int argc, char *argv[]) {
+    size_t numElements = 4 * 1024 * 1024;
+    size_t numIterations = 8;
+
+    size_t *data;
+    checkCudaError(cudaMallocManaged(&data, numElements * sizeof(size_t)));
+
+    initializeData(data, numElements);
+
+    //# main 'work'
+    for (int it = 0; it < numIterations; ++it) {
+        increase(data, numElements);
+    }
+
+    verifyData(data, numElements, numIterations);
+
+    checkCudaError(cudaFree(data));
+}
